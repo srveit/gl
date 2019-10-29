0331307830ba03792edf8090462e7351aaf6a2f9
Stephen R. Veit <veits@objectcomputing.com>
2019-10-29 15:13:50 -0500

    add chord parameter to sphere

--- geometry/sphere.mjs
+++ geometry/sphere.mjs
@@ -4,9 +4,9 @@ import { memoize } from '../internal/utils.mjs';
 const PI = Math.PI;
 const PI2 = PI * 2;
 
-function create_smooth_geometry(turns, bands) {
+function create_smooth_geometry(turns, bands, turns_chord, bands_chord) {
 	const num_vertices = (turns + 1) * (bands + 1);
-	const num_faces_per_turn = 2 * (bands - 1);
+	const num_faces_per_turn = 2 * bands;
 	const num_faces = num_faces_per_turn * turns;
 
 	const position = new Float32Array(num_vertices * 3); // doubles as normal
@@ -17,10 +17,10 @@ function create_smooth_geometry(turns, bands) {
 	let uv_index = 0;
 
 	for (let i = 0; i <= turns; i += 1) {
-		const u = i / turns;
+		const u = i / turns * turns_chord;
 
 		for (let j = 0; j <= bands; j += 1) {
-			const v = j / bands;
+			const v = j / bands * bands_chord;
 
 			const x = -Math.cos(u * PI2) * Math.sin(v * PI);
 			const y = Math.cos(v * PI);
@@ -45,7 +45,7 @@ function create_smooth_geometry(turns, bands) {
 		index[face_index++] = offset + 1;
 		index[face_index++] = offset + bands + 2;
 
-		for (let j = 1; j < bands - 1; j += 1) {
+		for (let j = 1; j < bands; j += 1) {
 			index[face_index++] = offset + j;
 			index[face_index++] = offset + j + 1;
 			index[face_index++] = offset + j + bands + 1;
@@ -78,12 +78,12 @@ function create_smooth_geometry(turns, bands) {
 	});
 }
 
-function create_flat_geometry(turns, bands) {
+function create_flat_geometry(turns, bands, turns_chord, bands_chord) {
 	throw new Error('TODO implement flat geometry');
 }
 
-export default memoize(({ turns = 8, bands = 6, shading = 'smooth' } = {}) => {
+export default memoize(({ turns = 8, bands = 6, turns_chord = 1, bands_chord = 1, shading = 'smooth' } = {}) => {
 	return shading === 'smooth'
-		? create_smooth_geometry(turns, bands)
-		: create_flat_geometry(turns, bands);
+		? create_smooth_geometry(turns, bands, turns_chord, bands_chord)
+		: create_flat_geometry(turns, bands, turns_chord, bands_chord);
 });
